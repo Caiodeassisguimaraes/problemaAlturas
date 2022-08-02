c61b607ab5046828a780298c9bd94fc272583ae0
Caio de Assis Guimaraes <caiodeassisguimaraes@hotmail.com>
2022-08-01 22:21:46 -0300

    ajustes finais

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 			System.out.println("Dados da " + (i + 1) + "a pessoa:");
 			System.out.print("Nome: ");
 			String name = sc.next();
-			System.out.print("Idade:");
+			System.out.print("Idade: ");
 			int age = sc.nextInt();
 			System.out.print("Altura: ");
 			double hight = sc.nextDouble();
@@ -31,7 +31,8 @@ public class Program {
 			sum += vector[i].getHight();
 		}
 
-		System.out.println(String.format("Altura média: %.2f",Subject.averageHight(sum, vector.length)));
+		//System.out.println(String.format("Altura média: %.2f",Subject.averageHight(sum, vector.length)));
+		System.out.printf("Altura média: %.2f%n", Subject.averageHight(sum, vector.length));
 		
 		int count = 0;
 		for (int i = 0; i < vector.length; i++) {
@@ -40,7 +41,7 @@ public class Program {
 			}
 		}
 
-		System.out.println("Pessoas com menos de 16 anos: " + Subject.menorDe16(count, vector.length) + "%");
+		System.out.println("Pessoas com menos de 16 anos: " + Subject.underSixteen(count, vector.length) + "%");
 
 		for (int i = 0; i < vector.length; i++) {
 			if (vector[i].getAge() < 16) {
